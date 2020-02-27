e3dfdd175e1a64d30ef4141846d1ea3d793221d9
schreyerpeter <pschreyer@weedmaps.com>
2020-02-27 13:45:09 -0800

    Building hashing functions

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -12,7 +12,15 @@ defmodule Identicon do
       :world
 
   """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_input
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
   end
 end
